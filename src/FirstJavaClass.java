1a66fb7e45a35fe38b53ca9e8bc77a25dc8e7f31
Kübra Tekin <kubratekin1611@gmail.com>
2022-03-26 22:55:19 +0300

    make changes

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -1,5 +1,6 @@
 public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("Learning github");
+        System.out.println("Adding a new code");
     }
 }
