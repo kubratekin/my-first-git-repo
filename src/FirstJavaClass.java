bf7a7213b83b4d19f9b39e028235b251ea92b3f1
Kübra Tekin <kubratekin1611@gmail.com>
2022-03-27 16:57:03 +0300

    third commit

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -2,5 +2,6 @@ public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("Learning github");
         System.out.println("Adding a new code");
+        System.out.println("Learn java earn money ");
     }
 }
